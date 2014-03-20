e7e49906efe8aa8252397857d158cab3e60c3094
Ankit Kamboj <er.ankitkamboj@gmail.com>
2014-03-21 02:23:13 +0530

    Test commit

---
+++ NewTest/src/com/akt/newtest/MyActivity.java
@@ -0,0 +1,17 @@
+package com.akt.newtest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MyActivity extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		
+		
+		setContentView(R.layout.activity_main);
+	}
+	
+}	
